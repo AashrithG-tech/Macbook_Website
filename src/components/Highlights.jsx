32d21310d3f9d76e3e43295ab0c535754787fa1a
AASHRITH <aashrith@Mac.lan>
2025-11-30 00:54:09 +0530

    Highlights implemented

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,66 @@
+import {useMediaQuery} from "react-responsive";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+
+const Highlights = () => {
+
+    const isMobile = useMediaQuery({query:'(max-width:1024px)'})
+
+    useGSAP(() => {
+        gsap.to(['.left-column','.right-column'],{
+            scrollTrigger:{
+                trigger: '#highlights',
+                start:isMobile ? 'bottom bottom' : 'top center',
+            },
+            y:0,
+            opacity:1,
+            stagger:0.5,
+            ease:'power1.inOut',
+            duration:1
+        })
+    })
+
+    return (
+        <section id="highlights">
+
+            <h2>There’s never been a better time to upgrade.</h2>
+            <h3>Here’s what you get with the new MacBook Pro.</h3>
+
+            <div className="masonry">
+                <div className="left-column">
+                    <div>
+                        <img src="/laptop.png" alt="Laptop" />
+                        <p>Fly through demanding tasks upto 9.8x faster</p>
+                    </div>
+
+                    <div>
+                        <img src="/sun.png" alt="Sun" />
+                        <p>A stunning <br />
+                           Liquid Retina XDR <br />
+                           Display.</p>
+                    </div>
+                </div>
+
+                <div className="right-column">
+                    <div className="apple-gradient">
+                        <img src="/ai.png" alt="AI" />
+                        <p>Built for <br />
+                           <span>Apple Intelligence</span></p>
+                    </div>
+
+                    <div>
+                        <img src="/battery.png" alt="Battery" />
+                        <p>Up to
+                            <span className="green-gradient">14 more hours {" "}</span>
+                            battery life. <span className="text-gray-500">(Upto 24 hours total)</span></p>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </section>
+    )
+}
+
+export default Highlights
